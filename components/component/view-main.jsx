7d9eadd4bed2a7932b272f78d76a2a2c7a2667c5
miusarname2 <oscarmauricio631@gmail.com>
2026-06-05 11:29:27 -0500

    feat: :sparkles: Upload the new app

--- components/component/view-main.jsx
+++ components/component/view-main.jsx
@@ -401,7 +401,7 @@ export function ViewMain() {
           {/* Project 8: GardenApp */}
           <div className="group flex flex-col bg-white dark:bg-gray-800 rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300 hover:-translate-y-1 border border-gray-100 dark:border-gray-700">
             <div className="w-full aspect-[4/3] overflow-hidden bg-gray-100 dark:bg-gray-900 relative">
-              <img alt="GardenApp" className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500" src="/placeholder.svg" />
+              <img alt="GardenApp" className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500" src="/GardenApp.png" />
             </div>
             <div className="p-6 flex flex-col flex-1">
               <h3 className="text-xl font-bold mb-3">GardenApp - Plant Monitor</h3>
